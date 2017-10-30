5fc24caff16258935306962806ec6c8c79208371
Nicholas Kern <nkern@berkeley.edu>
2017-10-29 20:31:47 -0700

    modified:   .gitignore 	modified:   setup.py 	modified:   vis_sim/__init__.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 import sys
 import os
+import subprocess
+
 try:
     from setuptools import setup
 except:
     from distutils.core import setup
 
+# write git-hash
+githash = subprocess.check_output(["git", "describe", "--always"]).strip()
+with open('vis_sim/hash.txt', 'w') as f:
+    f.write(str(githash))
+
+# setup vars
 setup(
     name            = 'vis_sim',
     version         = '0.1',
--- vis_sim/__init__.py
+++ vis_sim/__init__.py
@@ -1,3 +1,11 @@
+# load modules
 from .beam_model import Beam_Model
 from .sky_model import Sky_Model
 from .vis_sim import Vis_Sim
+
+# try to load githash
+try:
+    with open('hash.txt', 'r') as f:
+        __githash__ = f.read()
+except:
+    __githash__ = ''
